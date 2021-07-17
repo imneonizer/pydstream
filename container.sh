1a105c489bb0f7a7bb9e68511012c107706e3cd9
smartcow <mneonizer@gmail.com>
2021-07-17 14:02:54 +0530

    Updated Dockerfile

--- container.sh
+++ container.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-if  [ "$EUID" -ne 0 ];then
-    echo "usage: sudo $0 [--build | --run | --attach | --kill]"
-    exit 1
+# Check if running with root and atleast one arg is passed
+if  [ "$EUID" -ne 0 ];then sudo -E $0 $@; exit 0; fi
+if  [ !$1 ];then echo "usage: $0 [--build | --run | --attach | --kill]"; exit 1; fi
+
+# select correct dockerfile based on platform
+if [ -f "/etc/nv_tegra_release" ];then
+    DOCKER_FILE="dockerfile/Dockerfile.l4t"
+else
+    DOCKER_FILE="dockerfile/Dockerfile.x86"
 fi
 
-NAME="pydstream"
-
+# function to echo and execute commands
 function call {
     echo ">> $1";$1
 }
 
+# default container name
+NAME="pydstream"
+
 if [[ $1 == "--build" || $1 == "-b" ]];then
-    call "docker build . -t $NAME"
+    # Build the container
+    call "docker build . -t $NAME -f $DOCKER_FILE"
 
 elif [[ $1 == "--run" || $1 == "-r" ]];then
+    # Run a new container
     export DISPLAY=:1
     xhost +
     call "docker run --rm -it --gpus all \
@@ -28,9 +38,11 @@ elif [[ $1 == "--run" || $1 == "-r" ]];then
     pydstream bash"
 
 elif [[ $1 == "--attach" || $1 == "-a" ]];then
+    # Attach to already running container
     call "docker exec -it $NAME bash"
 
 elif [[ $1 == "--kill" || $1 == "-k" ]];then
+    # Kill any other existing container
     call "docker kill $NAME"
 
 fi
