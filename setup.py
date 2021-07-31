10833a8e45e3513903b0231b4dac4e78fdd0300c
nitin <mneonizer@gmail.com>
2021-07-31 10:29:36 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
-from setuptools import setup, Extension
-from setuptools import find_packages
+from distutils.core import setup
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     setup(
-        name="pydstream",
-        version="0.0.1",
-        description="Easy to use python wrapper around Deepstream Python bindings.",
-        long_description_content_type="text/markdown",
-        author="Nitin Rai",
-        author_email="mneonizer@gmail.com",
-        url="https://github.com/imneonizer/pydstream",
-        license="MIT License",
-        platforms=["linux", "unix"],
-        python_requires=">3.5.2",
+        name = 'pydstream',
+        packages = ['pydstream'],
+        version = '0.0.1',
+        license='MIT',
+        description = 'Easy to use python wrapper around Deepstream Python bindings',
+        author = 'Nitin Rai',
+        author_email = 'mneonizer@gmail.com',
+        url = 'https://github.com/imneonizer/pydstream',
+        keywords = ['python deepstream', 'pyds', 'deepstream', 'nvidia'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+        ],
     )
